011628b965f9240ab5892c71034264e4726e05a9
test <join27833@gmail.com>
2021-08-02 18:16:25 +0900

    2021-08-02

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
-import React from "react";
-import Potato from "./potato";
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+import Movie from './Movie';
 
-function App() {
-  return (
-    <div className="App">
-    <h1>hello</h1>
-    <Potato></Potato>
-    </div>
-  );
+
+function Food({name,picture}){
+  
+  return (<div>
+<h2>I like {name}</h2>
+<img src={picture}
+
+/>  
+</div>  
+); 
+ 
 }
 
-export default App;
+const foodIlike = [
   
+{
+name:"kimchi", 
+id:3,
+image : 
+"http://image.genie.co.kr/Y/IMAGE/IMG_ARTIST/067/872/918/67872918_1616652768439_20_600x600.JPG"
+
+},
+{
+name:"kimcha",
+id:1,
+image : 
+"http://image.genie.co.kr/Y/IMAGE/IMG_ARTIST/067/872/918/67872918_1616652768439_20_600x600.JPG"
+
+}
+
+
+
+];
+
+function renderFood(dish){
+return <Food key={dish.id} name={dish.name} picture={dish.image}/>
+}
+
+function App(){ 
+  return(
+    <div>
+      {console.log(foodIlike.map(renderFood))}
+      {foodIlike.map(renderFood)}
+      </div>
+  );
+}  
+export default App;  
