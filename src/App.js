94be3bec126564692a64c0a98836bf20859af8bd
test <join27833@gmail.com>
2021-08-07 23:32:08 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import { render } from '@testing-library/react';
 import React, { Component } from 'react';
 import Movie from './Movie';
+import propTypes from "prop-types";
 
 
-function Food({name,picture}){
+function Food({name,picture,rating}){
   
   return (<div>
 <h2>I like {name}</h2>
+<h4>{rating} / 5.0</h4>
 <img src={picture}
 
 />  
-</div>  
+</div>   
 ); 
  
 }
+
+Food.propTypes = {
+  name:propTypes.string.isRequired,
+  picture:propTypes.string.isRequired,
+  rating:propTypes.number.isRequired
+};
 
 const foodIlike = [
   
@@ -21,15 +29,16 @@ const foodIlike = [
 name:"kimchi", 
 id:3,
 image : 
-"http://image.genie.co.kr/Y/IMAGE/IMG_ARTIST/067/872/918/67872918_1616652768439_20_600x600.JPG"
+"http://image.genie.co.kr/Y/IMAGE/IMG_ARTIST/067/872/918/67872918_1616652768439_20_600x600.JPG",
+rating : 5
 
 },
 {
 name:"kimcha",
 id:1,
 image : 
-"http://image.genie.co.kr/Y/IMAGE/IMG_ARTIST/067/872/918/67872918_1616652768439_20_600x600.JPG"
-
+"http://image.genie.co.kr/Y/IMAGE/IMG_ARTIST/067/872/918/67872918_1616652768439_20_600x600.JPG",
+rating:3
 }
 
 
@@ -37,14 +46,14 @@ image :
 ];
 
 function renderFood(dish){
-return <Food key={dish.id} name={dish.name} picture={dish.image}/>
+return <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
 }
 
 function App(){ 
   return(
     <div>
       {console.log(foodIlike.map(renderFood))}
-      {foodIlike.map(renderFood)}
+      {foodIlike.map(renderFood)}  
       </div>
   );
 }  
