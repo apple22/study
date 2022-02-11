9731ea79fe1ff480f4a6e8e12930d9c79da80f5b
apple22 <join27833@gmail.com>
2022-02-11 16:49:00 +0900

    study_01

---
+++ crash_2021/test_01/index.js
@@ -0,0 +1,92 @@
+
+        // Find the latest version by visiting https://cdn.skypack.dev/three.
+      
+        import * as THREE from 'https://unpkg.com/three@0.126.1/build/three.module.js';
+        import * as dat from 'dat.gui'
+
+const gui = new dat.GUI()
+const world = {
+       plane : {
+               width: 10,
+               height: 10,
+               widthSegments:10,
+               heightSegments:10
+       } 
+}
+
+function generatePlane(){
+        PlaneMesh.geometry.dispose()
+        PlaneMesh.geometry = new THREE.PlaneGeometry(
+                world.plane.width,
+                world.plane.height,
+                world.plane.widthSegments,
+                world.plane.heightSegments,
+                10,10)
+
+        const {array} = PlaneMesh.geometry.attributes.position
+
+        for(let i=0; i<array.length;i+=3){
+                const x = array[i]
+                const y = array[i + 1 ]
+                const z = array[i + 2 ]
+               
+                array[i + 2] = z + Math.random() 
+ 
+        }
+}
+
+gui.add(world.plane,'width',1,20).onChange(generatePlane)
+gui.add(world.plane,'height',1,20).onChange(generatePlane)
+gui.add(world.plane,'widthSegments',1,20).onChange(generatePlane)
+gui.add(world.plane,'heightSegments',1,20).onChange(generatePlane)      
+
+
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(
+        75 , innerWidth / innerHeight , 0.1, 1000);
+
+        const renderer = new THREE.WebGLRenderer();
+      
+        
+        renderer.setSize(innerWidth,innerHeight)
+        renderer.setPixelRatio(devicePixelRatio)
+        document.body.appendChild(renderer.domElement)
+
+      
+        const planeGeometry = new THREE.PlaneGeometry(10,10,10,10)
+        const PlaneMaterial = new THREE.MeshPhongMaterial({color:0xff0000,
+        side:THREE.DoubleSide,
+        flatShading:THREE.FlatShading
+        })
+        const PlaneMesh = new THREE.Mesh(planeGeometry,PlaneMaterial)
+        
+       console.log(PlaneMesh.geometry.attributes.position.array)
+     
+
+        const light = new THREE.DirectionalLight(0xffffff,1)
+        light.position.set(0,0,1)
+        scene.add(light)
+   
+
+        scene.add(PlaneMesh)
+
+
+     
+
+       
+        camera.position.z = 5
+
+        function animate(){
+                requestAnimationFrame(animate)
+                renderer.render(scene,camera)
+                //PlaneMesh.rotation.x += 0.01
+              
+        }
+
+        animate()
+
+
+        renderer.render(scene,camera)
+        
+      
+        
