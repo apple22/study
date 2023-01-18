c44f16580ebd881a3aab5450a90bc1189901183f
apple22 <join27833@gmail.com>
2023-01-19 05:35:08 +0900

    2023-01-19

---
+++ variable/variable4.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$a = 1000;
+$a = $a + 10;
+print $a. '<br/>';
+$a = $a / 10;
+print $a. '<br/>';
+$a = $a - 10;
+print $a. '<br/>';
+$a = $a * 10;
+print $a. '<br/>';
+
+?>
+</body>
+</html>
