0c9c4682ce2098d981c655c80c75366c1bdee40c
apple22 <join27833@gmail.com>
2024-08-09 17:27:41 +0900

    2024-08-09

---
+++ js/header.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    includeHTML(); // Include HTML content
+
+    // 이벤트 핸들러는 includeHTML 호출 이후에 설정해야 함
+    $(document).on('mouseenter', 'ul.gnb_ul', function() {
+        
+    });
+});
