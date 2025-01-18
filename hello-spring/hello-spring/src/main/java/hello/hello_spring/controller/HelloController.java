23f31479b0c856c3b89a3794e52fe75d60f9cd13
apple22 <join27833@gmail.com>
2025-01-18 14:39:15 +0900

    MVC와 템플릿 엔진

---
+++ hello-spring/hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -0,0 +1,24 @@
+package hello.hello_spring.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+
+    @GetMapping("/hello")
+    public String Hello(Model model) {
+        model.addAttribute("data", "hello!");
+        return "hello";
+    }
+
+    @GetMapping("/hello-mvc")
+    public String hyejin(@RequestParam(value = "name" , required = true) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-mvc";
+    }
+
+}
